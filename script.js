42ae128b4e97ed0884b4c2a132bad42b038eb03a
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-01-08 10:17:29 -0600

    prompting user for num chars

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var numCharsForPassword = 0;  // Number of characters in the password
+  
+  
+
+  do {
+    numCharsForPassword = prompt("Please enter the number of characters for your password. Please note that the number of characters must be between 8 and 128.");
+    console.log("Num chars: " + numCharsForPassword); // Debug
+  } 
+  while ((numCharsForPassword < 8) ||
+         (numCharsForPassword > 128));
+  //        (typeof numCharsForPassword !== "number"));
+
+
+}
 
 // Write password to the #password input
 function writePassword() {
