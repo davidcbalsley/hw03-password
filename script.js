9585d424b968114364a22b42ec1568441aeaa3bd
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-01-09 17:19:25 -0600

    show error message if input is invalid

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ function generatePassword() {
   var numCharsForPassword = 0;        // Number of characters in the password
   var includeSpecialChars = false;    // Whether to include special characters in passwrod
   var includeNumericChars = false;    // Whether to include numbers in password
-  var includeLowerCaseChars = fales;  // Whether to include lowercase letters in password
+  var includeLowerCaseChars = false;  // Whether to include lowercase letters in password
   var includeUpperCaseChars = false;  // Whether to include uppercase letters in password
   
   do {
     numCharsForPassword = prompt("Please enter the number of characters for your password. Please note that the number of characters must be between 8 and 128.");
     console.log("Num chars: " + numCharsForPassword); // Debug
+
+    // If input value is unacceptable, alert the user
+    if ((numCharsForPassword < 8) || (numCharsForPassword > 128)) {
+      alert("Please enter a number between 8 and 128.");
+    } else if (isNaN(numCharsForPassword)) {
+      alert("Please enter a number.")
+    }
   } 
   while ((numCharsForPassword < 8) ||
          (numCharsForPassword > 128) ||
