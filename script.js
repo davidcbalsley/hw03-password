c43944e0d6a1eead18921ce0b5cd1624c48d9bbb
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-01-14 05:55:50 -0600

    making copy button disabled

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var copyToClipboardBtn = document.querySelector("#copy-to-clipboard");
+var copyBtn = document.querySelector("#copy-to-clipboard");
 
-var charsForPassword = [
+function getRandomInt(maxValue) {
+  return Math.floor(Math.random() * maxValue);
+}
+
+function getRandomChar(inputString) {
+  var valueIndex = 0;   // Randomly generated index into string
+
+  // Get a random index into the values array
+  valueIndex = getRandomInt(inputString.length - 1);
+
+  return inputString[valueIndex];
+}
+
+function generatePassword() {
+  var numCharsForPassword = 0;                // Number of characters in the password
+  var newPassword = "";                       // The new password that we are creating
+  var numSelectedCharCategories = 0;          // The number of categories of characters the user has selected
+  var includeCurrentCharCategory = false;     // Whether the user has elected to include the current category of characters
+  var indicesForSelectedCharCategories = [];  // The indices in 'charsForPassword' for the categories that the user has selected
+  var randomCharIndex = 0;                    // Randomly generated index into indicesForSelectedCharCategories 
+  var nextCharForPassword = "";               // The next char to append to the end of the password
+
+  // Categories of characters and their descriptors
+  var charsForPassword = [
   {
     descriptor: "special characters",
     values: "!#$%&()*+-./:;?@~"
@@ -20,35 +43,10 @@ var charsForPassword = [
     values: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   }
 ];
-
-function getRandomInt(maxValue) {
-  return Math.floor(Math.random() * maxValue);
-}
-
-function getRandomChar(charTypeIndex) {
-  var valueIndex = 0;   // Index into the values array for the current object
-
-  // Get a random index into the values array
-  valueIndex = getRandomInt(charsForPassword[charTypeIndex].values.length - 1);
-  // console.log("charTypeIndex: " + charTypeIndex); // debug
-  // console.log("valueIndex: " + valueIndex); // debug
-
-  return charsForPassword[charTypeIndex].values[valueIndex];
-}
-
-function generatePassword() {
-  var numCharsForPassword = 0;                // Number of characters in the password
-  var newPassword = "";                       // The new password that we are creating
-  var numSelectedCharCategories = 0;          // The number of categories of characters the user has selected
-  var includeCurrentCharCategory = false;     // Whether the user has elected to include the current category of characters
-  var indicesForSelectedCharCategories = [];  // The indices in 'charsForPassword' for the categories that the user has selected
-  var randomCharIndex = 0;                    // Randomly generated index into indicesForSelectedCharCategories 
-  var nextCharForPassword = "";               // The next char to append to the end of the password
   
   // Prompt the user to enter the number of characters for the password
   do {
     numCharsForPassword = prompt("Please enter the number of characters for your password. Please note that the number of characters must be between 8 and 128.");
-    // console.log("Num chars: " + numCharsForPassword); // Debug
 
     // If input value is unacceptable, alert the user
     if ((numCharsForPassword < 8) || (numCharsForPassword > 128)) {
@@ -63,17 +61,12 @@ function generatePassword() {
 
   // Prompt the user to pick the types of characters to include in the password
   do {
-    // console.log("do while for picking types of chars"); // debug
-
     for (var i = 0; i < charsForPassword.length; i++) {
-      // console.log("for loop for confirming categories"); // debug
       includeCurrentCharCategory = confirm("Click OK to confirm including " + charsForPassword[i].descriptor + ".");
       
       if (includeCurrentCharCategory) {
         indicesForSelectedCharCategories.push(i);
-        // console.log("indicesForSelectedCharCategories: " + indicesForSelectedCharCategories); // debug
         numSelectedCharCategories++;
-        // console.log("numSelectedCharCategories: " + numSelectedCharCategories); // debug
       }
     }
 
@@ -87,13 +80,10 @@ function generatePassword() {
   while (newPassword.length < numCharsForPassword) {
     // Randomly choose the next category of characters
     randomCharIndex = indicesForSelectedCharCategories[getRandomInt(numSelectedCharCategories)];
-    // console.log("randomCharIndex: " + randomCharIndex); // debug
 
     // Get random character from category and append it to new password
-    nextCharForPassword = getRandomChar(randomCharIndex);
-    // console.log("nextCharForPassword: " + nextCharForPassword); // debug
+    nextCharForPassword = getRandomChar(charsForPassword[randomCharIndex].values);
     newPassword += nextCharForPassword;
-    // console.log("newPassword: " + newPassword); // debug
   }
 
   return newPassword;
@@ -128,4 +118,4 @@ function copyToClipboard() {
 generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
-copyToClipboardBtn.addEventListener("click", copyToClipboard);
+copyBtn.addEventListener("click", copyToClipboard);
